acafafaca4343510e5a23395e9dbf0e36fb69408
Pawel Suminski <p.suminski@gmail.com>
2014-12-25 18:42:45 +0100

    dzialajacy PID

--- XVXVX/XVXVX.c
+++ XVXVX/XVXVX.c
@@ -1,66 +1,96 @@
 //192.168.1.103
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <sched.h>
+#include <time.h>
 
-int i = 12;
-char ci[10];
+#define OBJ 0
+#define PROC 1
+#define PID 2
 
-float T = 10;
+float Kp = 2, Ti = 1;
+double T = 10;
+double Tp = 0;
+double dt = 0;
 int k = 1;
-float y = 0;
-float y1 = 0;
-int x = 0;
-int _x = 0;
+float y = 0, y1 = 0, e = 0, e0 = 0, de = 0, ie = 0, st = 0;
+int x = 0, _x = 0;
+clock_t t0, t1;
 char _y[10];
 
-void doprocessing (int sock)
+
+pthread_t threads[3];
+pthread_mutex_t mutex;
+
+void *inertial(void){
+	while(1){
+
+		//t0 = clock();
+		//dt = t0-t1;
+		//Tp = (double)(dt/CLOCKS_PER_SEC);
+		//printf("t0:%lu t1:%lu\n", t0, t1);
+		y = (1/T)*(k*st-y1)+y1;
+		y1 = y;
+		//t1 = clock();
+		//printf("y:%f x:%d\n", y, x);
+		delay(100);
+	}
+}
+void *pid(void){
+	while(1){
+		e = x - y;
+		ie += e;
+		de = e - e0;
+		e0 = e;
+		st = Kp*(e+ie);
+
+		delay(100);
+	}
+}
+void *doprocessing (int sock)
 {
+	while(1){
+		int n;
+		char buffer[256];
+		bzero(buffer,256);
 
-    int n;
+		n = read(sock, &_x, 4, 0);
+		if (n < 0){
+			perror("ERROR reading from socket");
+			exit(1);
+		}
 
-    char buffer[256];
+		x = ntohl(_x);
+		itoa((int)(y*100), _y, 10);
+		n = send(sock, &_y, 10, 0);
 
-    bzero(buffer,256);
-
-    //n = read(sock,buffer,255);
-    n = read(sock, &_x, 4, 0);
-    x = ntohl(_x);
-    //printf("x: %d\n", _x);
-    if (n < 0)
-    {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-    //printf("Message: %s\n",buffer);
-
-    y = (1/T)*(k*x-y1)+y1;
-    y1 = y;
-    printf("y:%f x:%d\n", y, x);
-    i++;
-    itoa(i, ci, 10);
-    itoa((int)(y*100), _y, 10);
-    n = send(sock, &_y, 10, 0);
-    //n = write(sock, (char *)i ,sizeof((char *)i));
-
-
-
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        exit(1);
-    }
+		if (n < 0){
+			perror("ERROR writing to socket");
+			exit(1);
+		}
+	}
 }
 
 int main( int argc, char *argv[] )
 {
-    int sockfd, newsockfd, portno, clilen;
+
+	int status;
+	pthread_attr_t attr;
+	pthread_attr_init( &attr );
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	pthread_mutex_init(&mutex, NULL);
+
+	int sockfd, newsockfd, portno, clilen;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
-    int  n, pid;
 
-    /* First call to socket() function */
+
+
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
@@ -75,9 +105,7 @@ int main( int argc, char *argv[] )
     serv_addr.sin_port = htons(portno);
 
     /* Now bind the host address using bind() call.*/
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                          sizeof(serv_addr)) < 0)
-    {
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
          perror("ERROR on binding");
          exit(1);
     }
@@ -87,19 +115,25 @@ int main( int argc, char *argv[] )
      */
     listen(sockfd,5);
     clilen = sizeof(cli_addr);
-    while (1)
-    {
-        newsockfd = accept(sockfd,
-                (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0)
-        {
-            perror("ERROR on accept");
-            exit(1);
-        }
-        /* Create child process */
-        while(1)
-            	doprocessing(newsockfd);
-            exit(0);
 
-    } /* end of while */
+    //while (1){
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	if (newsockfd < 0){
+		perror("ERROR on accept");
+		exit(1);
+	}
+	pthread_create(&threads[OBJ], &attr, inertial, NULL);
+	pthread_create(&threads[PROC], &attr, doprocessing, newsockfd);
+	pthread_create(&threads[PID], &attr, pid, NULL);
+	pthread_join(threads[PROC], (void **)&status);
+	pthread_mutex_destroy(&mutex);
+	pthread_attr_destroy(&attr);
+
+        /* Create child process */
+       // while(1)
+            	//doprocessing(newsockfd);
+            //exit(0);
+
+    //} /* end of while */
+    return 0;
 }
